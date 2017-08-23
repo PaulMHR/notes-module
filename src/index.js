b0a012e9da8022da051385144fa612438b79a240
Paul Rudmik <paul.rudmik@gmail.com>
2017-08-23 15:49:07 -0400

    Configured basic CSS and eliminated superfluous code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import firebase from 'firebase';
-//import MainMenu from './components/main/index';
 import MainRoute from './mainRoute';
-//import SubjectNotes from './components/note/subjectNotes';
-//import initializeNotes from './redux/notes.js';
 
 // Initialize Firebase
 const config = {
@@ -18,9 +15,6 @@ const config = {
 firebase.initializeApp(config);
 
 ReactDOM.render(
-	//<SubjectNotes subject="ENGL 200A" />,
 	<MainRoute />,
     document.getElementById('root')
 );
-
-//initializeNotes();
--- src/mainRoute.jsx
+++ src/mainRoute.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import './styles/main.css';
 
 import MainMenu from './components/main/index';
 import Note from './components/note/index';
 
-
 export default () => (
     <Router>
-        <div>
+        <div className="mainDiv">
             <Route exact={true} path="/">
                 <MainMenu />
             </Route>
