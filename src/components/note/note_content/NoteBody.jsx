d41aa930e14f8efa8b381ff36424c76974ae498d
Paul Rudmik <paul.rudmik@gmail.com>
2017-10-16 21:59:29 -0400

    MVI1: generalized logical control flow from Firebase

--- src/components/note/note_content/NoteBody.jsx
+++ src/components/note/note_content/NoteBody.jsx
@@ -3,11 +3,16 @@ import React from 'react';
 import "../styles/NoteBody.css";
 
 // TODO figure out what to do with date ;~;
-export default ({first_unit_note}) => {
-	console.log(first_unit_note);
+export default ({date, date_note}) => {
 	return (
 		<div className="note_body_container">
-            {first_unit_note.map(function(content_item, i) {
+			<div className="date_div">
+				{date !== "null" ?
+					<p>{date}</p> :
+					<span></span>
+				}
+			</div>
+            {date_note.map(function(content_item, i) {
                 return <p key={i}>{content_item}</p>;
             })}
 		</div>
--- src/components/note/note_content/index.jsx
+++ src/components/note/note_content/index.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import NoteHeader from './NoteTitle';
 import NoteBody from './NoteBody';
 
-export default ({title, unit_content}) => (
-	<div>
-		<NoteHeader title={title} />
-		<NoteBody first_unit_note={unit_content["null"]} />
-	</div>
-);
+export default ({title, unit_content}) => {
+    const dates = Object.keys(unit_content);
+    return (
+        <div>
+            <NoteHeader title={title} />
+            {dates.map((date, i) => <NoteBody key={i} date={date} date_note={unit_content[date]} />)}
+        </div>
+    );
+}
