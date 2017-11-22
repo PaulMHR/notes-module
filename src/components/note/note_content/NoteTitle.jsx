618c1ccd3433e5e8856d0c7440da287ee2440ae4
Paul Rudmik <paul.rudmik@gmail.com>
2017-11-21 21:53:33 -0500

    MVI2: stylistic changes to make notes seem more appealing to the eye. Also dealt with some numbered lists in notes that were being entered in Markdown as though they were headers.

--- src/components/note/note_content/NoteTitle.jsx
+++ src/components/note/note_content/NoteTitle.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 
 import "../styles/NoteTitle.css";
 
-export default ({title, date}) => (
-	<div>
-		<ul className="note_title_list">
-			<li><p className="note_title_name"><strong>{title}</strong></p></li>
-			<li className="note_title_date"><p>{date}</p></li>
-		</ul>
+export default ({title}) => (
+	<div className="note-content-title-container">
+		<h1 className="note-title">{title}</h1>
+		<hr/>
 	</div>
 );
--- src/components/shared/BoxLink.jsx
+++ src/components/shared/BoxLink.jsx
@@ -8,7 +8,7 @@ export default ({courseId, courseTitle, imagePath}) => (
     <div id="box_link_outer" className="box_link_outer">
         <Link to={((getEnvironment() === "PROD") ? "/notorious" : "") + "/note/" + courseId} style={{'textDecoration': 'none'}}>
             <div>
-                <img src={"images/" + courseId.toLowerCase() + ".jpg"} alt={"Background image for " + courseId}/>
+                <img src={"images/" + courseId.toLowerCase() + ".jpg"} alt={"Background image for " + courseId.replace('_', ' ')}/>
                 <span className="box_link_inner">
                     <p className="box_link_id">{courseId.replace('_', ' ')}</p>
                     <p className="box_link_title">{courseTitle}</p>
